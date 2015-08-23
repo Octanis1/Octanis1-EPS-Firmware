d525ccc0fa1fd693290744b1f58961aa033ce69b
quentin.cabrol@gmail.com <quentin.cabrol@gmail.com>
2015-08-23 18:34:41 -0500

    starting I2C_Slave implementation --> some symbols undefined in msp430g2744.h to be redifined for the code to compile

--- EPS_dev/Debug/main.pp
+++ EPS_dev/Debug/main.pp
@@ -6,6 +6,7 @@ main.obj: /opt/ti/ccsv6/ccs_base/msp430/include/msp430g2744.h
 main.obj: /opt/ti/ccsv6/ccs_base/msp430/include/in430.h
 main.obj: /opt/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics.h
 main.obj: /opt/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_legacy_undefs.h
+main.obj: ../TI_USCI_I2C_slave.h
 
 ../main.cpp: 
 /opt/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
@@ -13,3 +14,4 @@ main.obj: /opt/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_le
 /opt/ti/ccsv6/ccs_base/msp430/include/in430.h: 
 /opt/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics.h: 
 /opt/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_legacy_undefs.h: 
+../TI_USCI_I2C_slave.h: 
